6b97d35af50b9782ac33cef00373a71e5e2852b1
Lionel De Swarte--Danko <lioneldeswarte--danko@lionels-mbp.mynet>
2021-03-11 16:44:36 +0000

    Start work on the rock-paper-scissors script, add a notebook to help with the work

---
+++ small-projects/rock-paper-scissors.py
@@ -0,0 +1,30 @@
+# This script allows you to play rock-paper-scissors against the computer
+# It asks for user input, displays randomly-generated output and keeps track of the score
+
+
+import random
+
+class Player:
+
+    def __init__(self):
+        self.name= input('Hello there, what is your name?')
+        self.score= 0
+
+
+    def play(self):
+        choice = input('Please select r for rock, p for paper or s for scissors')
+        return choice
+
+def winner(Player.play(), computer-choice):
+    pass
+
+lionel = Player()
+computer_score=0
+
+lionel.play()
+
+computer-choice = random.randint(0,2)
+
+
+
+    
