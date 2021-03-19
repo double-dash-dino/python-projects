93ab4d1ea20703fe6e0868e6397cddfaa9057a2e
Lionel De Swarte--Danko <lioneldeswarte--danko@lionels-mbp.mynet>
2021-03-19 18:10:05 +0000

    Get messaging started

--- medium-projects/reddit-program.py
+++ medium-projects/reddit-program.py
@@ -2,14 +2,23 @@
 # with the top reddit posts of the day.
 
 import getpass
+# To enter sensitive information
 import praw
+# For reddit API
+import smtplib
+# For email
 
+
+# Getting the information
 
 client_id=getpass.getpass('Please enter your client id')
 client_secret=getpass.getpass('Please enter your client secret')
 user_agent=getpass.getpass('Please enter your user agent')
 username=getpass.getpass('Please enter your username')
 password=getpass.getpass('Please enter your password')
+
+
+# Generating reddit object
 
 reddit = praw.Reddit(
 client_id=client_id, 
@@ -18,11 +27,22 @@ user_agent=user_agent,
 username=username, 
 password=password)
 
+# Generating list of posts
+
 subreddit_of_interest = input('What subreddit would you like to hear about?')
 
 subred = reddit.subreddit(subreddit_of_interest)
 
 top10 = subred.top("week", limit=10)
+
+# Setting up the email
+
+smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
+
+
+# Sending the email TODO
+
+# Printing the posts (will delete eventually)
 
 print(f'***************\nHere is a list of the top 10 posts for this week in r/{subreddit_of_interest}:')
 
